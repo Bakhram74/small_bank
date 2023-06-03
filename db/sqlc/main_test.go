782bc3ad9da1c422e005e280efef3c7d0c6398ce
Bakhram <bakhram7493@gmail.com>
2023-06-03 04:34:48 +0300

    update sqlc.yaml & added main_test.go

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:1234@localhost:5436/small_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+
+	testDB, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
