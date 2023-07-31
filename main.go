f787c01066d25bb7a0037be04cfae8dec0b7213d
Bakhram <bakhram7493@gmail.com>
2023-07-31 12:22:43 +0300

    DB migrations directly inside Golang code

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/Bakhram74/small_bank/gapi"
 	"github.com/Bakhram74/small_bank/pb"
 	"github.com/Bakhram74/small_bank/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -29,14 +32,27 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+	runDBMigration(config.MigrationURL, config.DBSource)
 	store := db.NewStore(conn)
 
 	go runGatewayServer(store, config)
 
 	runGrpcServer(store, config)
-	//runGinServer(store, config)
-}
 
+}
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance ", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+
+		log.Fatal("failed to run migrate up ", err)
+	}
+
+	log.Println("db migrated successfully")
+}
 func runGrpcServer(store db.Store, config util.Config) {
 	server, err := gapi.NewServer(store, config)
 	if err != nil {
