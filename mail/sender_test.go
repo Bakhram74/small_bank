ed754a299b25d8f99bb6c924c6b7686bfe6ee453
Bakhram <bakhram7493@gmail.com>
2023-08-04 12:13:10 +0300

    send email with gmail

---
+++ mail/sender_test.go
@@ -0,0 +1,23 @@
+package mail
+
+import (
+	"github.com/Bakhram74/small_bank/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	subject := "A test email"
+	content := `
+<h1>Hello World<h1>
+<p>This is a test message from <a href="http://techschool.guru">Tech School</a></p>
+`
+	to := []string{"bakhram7493@gmail.com "}
+	attachFiles := []string{"../Dockerfile"}
+	err = sender.SendMail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+
+}
